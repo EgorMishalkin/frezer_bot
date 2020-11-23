614cb5b4c37c200464ae1ad3924aa5e1cdc64650
EgorMishalkin <57597095+EgorMishalkin@users.noreply.github.com>
2020-11-23 15:54:42 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # словарь со всеми продуктами
-freezer = {}
+freezer = {'сок': 30,
+           'яблоко': 25,
+           'котлеты': 1,
+           'суп': 1}
 # словарь с отсортированными значениями
 sorted_freezer = {}
 com = ''
@@ -9,12 +12,19 @@ com = ''
 
 def sort():
     for i in sorted(freezer.items(), key=lambda para: para[1]):
-        sorted_freezer = i
-        print(sorted_freezer)
+        # sorted_freezer[i[0]] = i[1]
+        # i[0] - еда    i[1] - сколько живет
+        sorted_freezer[i[0]] = i[1]
+
+    for i in sorted(sorted_freezer.items(), key=lambda para: para[1]):
+        # sorted_freezer[i[0]] = i[1]
+        # i[0] - еда    i[1] - сколько живет
+        sorted_freezer[i[0]] = i[1]
+        if sorted_freezer[i[0]] == 1:
+            print('срок годности продукта истек')
+
 
 # функция добавления продукта
-
-
 def add(what, how_long):
     freezer[what] = how_long
     print('Продукт добавлен!')
@@ -66,6 +76,7 @@ while com != "стоп":
 
     elif 'сорт' in com.lower():
         sort()
+        print(sorted_freezer)
 
     # завершение
     elif 'стоп' in com.lower():
