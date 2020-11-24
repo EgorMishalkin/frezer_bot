76de22eed175b7330a43500a69fc8da118426a1b
EgorMishalkin <57597095+EgorMishalkin@users.noreply.github.com>
2020-11-24 16:14:33 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import telebot
 from telebot import types
 bot = telebot.TeleBot('1415966035:AAG6D1ybRJWEO5JNQvWR7pV0c8gTzJ6KRDI')
+bot.message_handler(commands=['start'])
 
 
-
-@bot.message_handler(commands=['start'])
 def start_message(message):
     keyboard1 = telebot.types.ReplyKeyboardMarkup()
     keyboard1.row('Добавить', 'Удалить')
@@ -13,13 +12,14 @@ def start_message(message):
                                       'а я буду следить чтобы они не протухли)', reply_markup=keyboard1)
 
 
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-    if message.text == 'Добавить':
-        bot.send_message(message.chat.id, "Введите название продукта: ")
-        
-    elif message.text == 'Удалить':
-        bot.send_message(message.chat.id, 'Удаление...')
+# функция отправки сообщения
+def send_text(message, text):
+    bot.send_message(message.chat.id, text)
 
+
+# функция получения
+@bot.message_handler(content_types=['text'])
+def gettext(message):
+    send_text(message, 'сообщение')
 
 bot.polling(none_stop=True)
