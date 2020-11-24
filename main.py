9aac44ff2b623b6b8ee4af31a7fcd78336be1dfc
EgorMishalkin <57597095+EgorMishalkin@users.noreply.github.com>
2020-11-24 16:48:32 +0400

    функции минимально работают

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import telebot
-from telebot import types
 
 bot = telebot.TeleBot('1415966035:AAG6D1ybRJWEO5JNQvWR7pV0c8gTzJ6KRDI')
 add_or_not = 0
+del_or_not = 0
+show_or_not = 0
+
+
+# функция отправки сообщения
+def send_text(message, text):
+    bot.send_message(message.chat.id, text)
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     # создание клавиатуры
     keyboard1 = telebot.types.ReplyKeyboardMarkup()
-    keyboard1.row('Добавить', 'Удалить')
+    keyboard1.row('/add', '/delete', '/show', '/help')
     # создание клавиатуры
     bot.send_message(message.chat.id, 'Привет, я твой холодильник \n'
                                       'напиши свои продукты и сколько они хранятся,'
@@ -18,27 +24,49 @@ def start_message(message):
 
 # функция добавления продукта
 @bot.message_handler(commands=['add'])
-def start_message(message):
+def add_product(message):
     global add_or_not
     bot.send_message(message.chat.id, 'Введите продукт и срок годности через пробел')
     add_or_not = 1
 
 
-# функция отправки сообщения
-def send_text(message, text):
-    bot.send_message(message.chat.id, text)
+# функция удаления продукта
+@bot.message_handler(commands=['delete'])
+def start_message(message):
+    global del_or_not
+    bot.send_message(message.chat.id, 'Введите продукт который надо удалить')
+    del_or_not = 1
+
+
+# функция показа всех продуктов
+@bot.message_handler(commands=['show'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'тут будем показывать словарь')
+
+
+# функция подсказки
+@bot.message_handler(commands=['help'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Бот, следящий за сроками продуктов')
 
 
 # функция получения
 @bot.message_handler(content_types=['text'])
 def gettext(message):
     global add_or_not
-    mess = message.text.split(' ')
+    global del_or_not
+    global show_or_not
+
     if add_or_not == 1:
+        mess = message.text.split(' ')
         send_text(message, mess[0])
         add_or_not = 0
+    if del_or_not == 1:
+        mess = message.text
+        send_text(message, 'delete')
+        del_or_not = 0
     else:
-        send_text(message, 'сообщение')
+        send_text(message, 'Неизвестная команда')
 
 
 bot.polling(none_stop=True)
