c09503727bf76f6b8e2d665500a390bc33947c53
EgorMishalkin <57597095+EgorMishalkin@users.noreply.github.com>
2020-11-25 23:22:09 +0400

    Update my_freezer.py

--- my_freezer.py
+++ my_freezer.py
@@ -1,24 +1,16 @@
 import telebot
 from telebot import types
 import sqlite3
+import sys
 
-
-# main
-# bot = telebot.TeleBot('1446473769:AAEI5XlIHW56g0yl-reM6x1ZP3ciy8MfOB8')
-bot = telebot.TeleBot('1415966035:AAG6D1ybRJWEO5JNQvWR7pV0c8gTzJ6KRDI')
-freezer1 = {}
-
-
-# функция отправки сообщения
-def send_text(message, text):
-    bot.send_message(message.chat.id, text)
+bot = telebot.TeleBot('1446473769:AAEI5XlIHW56g0yl-reM6x1ZP3ciy8MfOB8')
 
 
 # функция подключния к бд
 def ensure_connection(func):
     def inner(*args, **kwargs):
         global conn
-        with sqlite3.connect('o.db', check_same_thread=False) as conn:
+        with sqlite3.connect('users.db', check_same_thread=False) as conn:
             kwargs['conn'] = conn
             res = func(*args, **kwargs)
         return res
@@ -37,28 +29,9 @@ def init_db(conn, force: bool = False):
 		CREATE TABLE IF NOT EXISTS users (
 			user_id     INTEGER NOT NULL,
 			freezer     TEXT,
-			add_or_not     INT,
-			del_or_not     INT
-		)
+			remain INTEGER)
 	''')
     conn.commit()
-
-
-# добавение значений в бд
-@ensure_connection
-def add_user(conn, user_id: int, freezer, add_or_not: int, del_or_not: int):
-    c = conn.cursor()
-    c.execute('INSERT INTO users (user_id, freezer, add_or_not, del_or_not) VALUES (?, ?, ?, ?)',
-              (user_id, freezer, add_or_not, del_or_not))
-    conn.commit()
-
-
-#
-@ensure_connection
-def list_db(conn, user_id: int, limit: int = 1):
-    c = conn.cursor()
-    c.execute('SELECT user_id FROM users WHERE user_id = ? ORDER BY user_id DESC LIMIT ?', (user_id, limit))
-    return c.fetchall()
 
 
 @bot.message_handler(commands=['start'])
@@ -67,21 +40,10 @@ def do_start(message):
     keyboard1 = telebot.types.ReplyKeyboardMarkup()
     keyboard1.row('/добавить', '/удалить')
     keyboard1.row('/список', '/помощь')
+    bot.send_message(message.chat.id, 'Привет, я твой холодильник \n'
+                                      'напиши свои продукты и сколько они хранятся,'
+                                      'а я буду следить чтобы они не протухли)', reply_markup=keyboard1)
     # создание клавиатуры
-    get_user = list_db(user_id=message.from_user.id)
-    try:
-        if get_user[0][0]:
-            bot.send_message(message.chat.id,
-                             'Приветствую, ' + str(message.from_user.first_name) + '. Вы уже существуете в базе',
-                             reply_markup=keyboard1)
-        else:
-            add_user(user_id=message.from_user.id, freezer='', add_or_not=0, del_or_not=0)
-            bot.send_message(message.chat.id, str(message.from_user.first_name) + ', регистрация прошла успешно!',
-                             reply_markup=keyboard1)
-    except IndexError:
-        add_user(user_id=message.from_user.id, freezer='', add_or_not=0, del_or_not=0)
-        bot.send_message(message.chat.id, str(message.from_user.first_name) + ', регистрация прошла успешно!',
-                         reply_markup=keyboard1)
 
 
 # функция подсказки пользователю
@@ -90,139 +52,38 @@ def help_user(message):
     bot.send_message(message.chat.id, 'Бот, следящий за сроками продуктов. \n' +
                      'Функции: \n' +
                      '/добавить - добавление вашего продукта \n' +
-                     '/удалить - удаление вашего продукта \n' +
-                     'Так же бот каждый день будет уведомлять вас о том, какие продукты стоит съесть быстрее, а какие выкинуть)')
-
-
-@bot.message_handler(commands=['список'])
-def food(message):
-    string = ''
-    # в
-    c = conn.cursor()
-    user = message.from_user.id
-    value = c.execute(f'SELECT * FROM users WHERE user_id = {user}').fetchone()
-    freezer = value[1]
-    if freezer == '':
-        bot.send_message(message.chat.id, 'В вашем холодильнике ничего нет. Скорее сходите закупиться!')
-    else:
-        c = conn.cursor()
-        user = message.from_user.id
-        value = c.execute(f'SELECT * FROM users WHERE user_id = {user}').fetchone()
-        freezer = value[1]
-        # выводим начение столбца freezer
-        bot.send_message(message.chat.id, 'Ваша еда в холодильнике:\n' + freezer)
+                     '/удалить - удаление вашего продукта')
 
 
 # функция добавления продукта
 @bot.message_handler(commands=['добавить'])
 def add_product(message):
-    bot.send_message(message.chat.id, 'Введите продукт и срок годности через пробел')
-    # если эта переменная равна 1, то мы понимаем, что надо выполнить функцию
+    sent = bot.send_message(message.chat.id, 'Введите продукт и срок годности через пробел')
+    bot.register_next_step_handler(sent, add_product_to_db)
+
+def add_product_to_db(message):
     c = conn.cursor()
-    user = message.from_user.id
-    value = c.execute(f'SELECT * FROM users WHERE user_id = {user}').fetchone()
-    add_or_not = value[2]
-    c.execute(f'UPDATE users SET add_or_not = 1 WHERE user_id = {user}')
+    prod, rem = message.text.split(' ')
+    sql = f'INSERT INTO users (user_id, freezer, remain) VALUES ({message.chat.id}, "{prod}", {rem})'
+    print(sql, file=sys.stderr)
+    c.execute(sql)
     conn.commit()
+    bot.send_message(message.chat.id, 'Thank you!')
 
 
-# функция удаления продукта
 @bot.message_handler(commands=['удалить'])
 def del_product(message):
-    food(message)
-    bot.send_message(message.chat.id, 'Введите продукт и срок годности через пробел который надо удалить')
-    # если эта переменная равна 1, то мы понимаем, что надо выполнить функцию
+    sent = bot.send_message(message.chat.id, 'Введите продукт, который надо удалить')
+    bot.register_next_step_handler(sent, del_product_from_db)
+
+def del_product_from_db(message):
     c = conn.cursor()
-    user = message.from_user.id
-    value = c.execute(f'SELECT * FROM users WHERE user_id = {user}').fetchone()
-    del_or_not = value[3]
-    c.execute(f'UPDATE users SET del_or_not = 1 WHERE user_id = {user}')
+    prod = message.text
+    sql = f'DELETE FROM users WHERE user_id = {message.chat.id} AND freezer = "{prod}"'
+    print(sql, file=sys.stderr)
+    c.execute(sql)
     conn.commit()
-
-
-# функция получения любого текста
-@bot.message_handler(content_types=['text'])
-def gettext(message):
-    c = conn.cursor()
-    user = message.from_user.id
-    value = c.execute(f'SELECT * FROM users WHERE user_id = {user}').fetchone()
-    add_or_not = value[2]
-    del_or_not = value[3]
-    freezer = value[1]
-    # тут происходит обработка добавления
-    if add_or_not == 1:
-        mess = message.text
-        food = ''
-        # mess[0:-1] - название продукта
-        for i in mess[0:-1].split(' '):
-            food += f'{i} '
-        food = food[0:-1]
-        how_long = mess.split(' ')[-1]
-        try:
-            a = freezer + mess + '\n'
-            c.execute(f'UPDATE users SET freezer = "{a}" WHERE user_id = {user}')
-            conn.commit()
-            send_text(message, 'Успешно добавлено!')
-
-            c = conn.cursor()
-            user = message.from_user.id
-            value = c.execute(f'SELECT * FROM users WHERE user_id = {user}').fetchone()
-            add_or_not = value[2]
-            c.execute(f'UPDATE users SET add_or_not = 0 WHERE user_id = {user}')
-            conn.commit()
-        # ошибка при вводе
-        except IndexError:
-            send_text(message, 'Введены неверные данные!')
-        add_or_not = 0
-
-    # тут происходит обработка удаления
-    elif del_or_not == 1:
-        mess = message.text
-        food = ''
-        # mess[0:-1] - название продукта
-        for i in mess[0:-1].split(' '):
-            food += f'{i} '
-        food = food[0:-1]
-        how_long = mess.split(' ')[-1]
-        try:
-            # c.execute(f'UPDATE users SET freezer = "{a}" WHERE user_id = {user}')
-            print(mess in freezer)
-            print(freezer == mess)
-            print(mess)
-            print(user)
-            c.execute(f'DELETE FROM users WHERE  freezer = "{mess}" AND user_id = {int(user)}')
-            # query = f'DELETE FROM users WHERE "{mess}" IN freezer AND user_id = {user}'
-            # print(query)
-            # c.execute(query)
-            conn.commit()
-            # mess = 'банан 4'
-            # mess[0:-1] - название продукта
-            # for i in mess[0:-1]:f
-            #	string += f'{i} '
-            # строка string[0:-1] убирает лишний пробел в конце
-            # freezer[string[0:-1]] = mess[-1]
-            # freezer = mess
-            send_text(message, 'Удалено успешно!')
-            c = conn.cursor()
-            user = message.from_user.id
-            value = c.execute(f'SELECT * FROM users WHERE user_id = {user}').fetchone()
-            del_or_not = value[3]
-            c.execute(f'UPDATE users SET del_or_not = 0 WHERE user_id = {user}')
-            conn.commit()
-
-            c = conn.cursor()
-            user = message.from_user.id
-            value = c.execute(f'SELECT * FROM users WHERE user_id = {user}').fetchone()
-            add_or_not = value[2]
-            c.execute(f'UPDATE users SET add_or_not = 0 WHERE user_id = {user}')
-            conn.commit()
-        # ошибка при вводе
-        except KeyError:
-            send_text(message, 'Неизвестная ошибка!')
-
-
-    else:
-        send_text(message, 'Неизвестная команда')
+    bot.send_message(message.chat.id, 'Thank you!')
 
 
 if __name__ == '__main__':
