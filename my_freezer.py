800d18d64069c878089a03920f69042561bcf5fb
EgorMishalkin <57597095+EgorMishalkin@users.noreply.github.com>
2020-11-26 11:25:24 +0400

    Update my_freezer.py

--- my_freezer.py
+++ my_freezer.py
@@ -2,9 +2,11 @@ import telebot
 from telebot import types
 import sqlite3
 import sys
+import pymorphy2
 
 bot = telebot.TeleBot('1446473769:AAEI5XlIHW56g0yl-reM6x1ZP3ciy8MfOB8')
 
+morph = pymorphy2.MorphAnalyzer()
 
 # функция подключния к бд
 def ensure_connection(func):
@@ -40,6 +42,7 @@ def do_start(message):
     keyboard1 = telebot.types.ReplyKeyboardMarkup()
     keyboard1.row('/добавить', '/удалить')
     keyboard1.row('/список', '/помощь')
+    keyboard1.row('/обновление')
     bot.send_message(message.chat.id, 'Привет, я твой холодильник \n'
                                       'напиши свои продукты и сколько они хранятся,'
                                       'а я буду следить чтобы они не протухли)', reply_markup=keyboard1)
@@ -53,7 +56,7 @@ def help_user(message):
                      'Функции: \n' +
                      '/добавить - добавление вашего продукта \n' +
                      '/удалить - удаление вашего продукта \n' +
-                     '/список - выводит все содержимое вашего холодьлиника)')
+                     '/список - выводит все содержимое вашего холодильника)')
 
 
 # функция добавления продукта
@@ -98,6 +101,48 @@ def del_product_from_db(message):
     c.execute(sql)
     conn.commit()
     bot.send_message(message.chat.id, 'Продукт удален!')
+
+
+@bot.message_handler(commands=['обновление'])
+def show_product(message):
+    c = conn.cursor()
+    prod = message.text
+    sql1 = f'UPDATE users SET remain = remain - 1 WHERE user_id = {message.chat.id}'
+    c.execute(sql1)
+    sql2 = f'DELETE FROM users WHERE user_id = {message.chat.id} AND remain = 0'
+    c.execute(sql2)
+    conn.commit()
+    sql = f'SELECT freezer, remain FROM users WHERE user_id = {message.chat.id}'
+    c.execute(sql)
+
+    non_pull = 0
+    for key, value in c.fetchall():
+        print(key, value)
+        p = morph.parse(key)[0]
+        if 'plur' in p.tag:
+            verb = 'испортятся'
+        else:
+            verb = 'испортится'
+        if value == 3:
+            non_pull = 1
+            bot.send_message(message.chat.id, f'{key} {verb} через 3 дня! поторопитесь съесть/выпить)')
+        elif value == 2:
+            non_pull = 1
+            bot.send_message(message.chat.id, f'{key} {verb} через 2 дня! поторопитесь съесть/выпить!!')
+        elif value == 1:
+            non_pull = 1
+            bot.send_message(message.chat.id, f'{key} {verb} завтра! поторопитесь съесть/выпить!!!')
+        elif value == 0:
+            non_pull = 1
+            if 'plur' in p.tag:
+                bot.send_message(message.chat.id, f'{key} испортились! обязательно выкиньте продукт')
+            else:
+                bot.send_message(message.chat.id, f'{key} испортился! обязательно выкиньте продукт')
+
+    if non_pull == 0:
+        bot.send_message(message.chat.id, 'Поздравляю, ни один продукт не протух! Вы большой молодец!')
+    else:
+        pass
 
 
 @bot.message_handler(commands=['список'])
